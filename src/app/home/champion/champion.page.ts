41b2c892cafc6f283efc7b984ab7add17e84d5e7
zequn <jzequn@outlook.com>
2019-08-16 03:35:17 +1200

    initial commit

---
+++ src/app/home/champion/champion.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-champion',
+  templateUrl: './champion.page.html',
+  styleUrls: ['./champion.page.scss'],
+})
+export class ChampionPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
