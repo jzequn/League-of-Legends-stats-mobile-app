2e706f96455d959dd2adfe4b1813eeaed226fa66
zequn <jzequn@outlook.com>
2019-08-17 21:50:19 +1200

    add about page to side nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,9 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomePageModule) },
-  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthPageModule) }
+  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthPageModule) },
+  { path: 'about', loadChildren: () => import('./about/about.module').then(m => m.AboutPageModule) },
+  // { path: 'about', loadChildren: './about/about.module#AboutPageModule' }
   // { path: 'search', loadChildren: './home/search/search.module#SearchPageModule' },
   // { path: 'champion', loadChildren: './home/champion/champion.module#ChampionPageModule' },
   // { path: 'item', loadChildren: './home/item/item.module#ItemPageModule' },
